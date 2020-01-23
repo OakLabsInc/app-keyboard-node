f3d54c0631daf0e76623674056e028aaed67516c
rjoffray <richard@joffray.com>
2020-01-23 07:44:07 -0800

    adding input-event

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-var keypress = require('keypress');
- 
-// make `process.stdin` begin emitting "keypress" events
-keypress(process.stdin);
- 
-// listen for the "keypress" event
-process.stdin.on('keypress', function (ch, key) {
-  console.log('got "keypress"', key);
-  if (key && key.ctrl && key.name == 'c') {
-    process.stdin.pause();
-  }
-});
- 
-process.stdin.setRawMode(true);
-process.stdin.resume();
+const InputEvent = require('input-event');
+
+const input = new InputEvent('/dev/input/event0');
+
+const keyboard = new InputEvent.Keyboard(input);
+
+keyboard.on('keyup'   , console.log);
+keyboard.on('keydown' , console.log);
+keyboard.on('keypress', console.log);
